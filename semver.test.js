1032b4fd6557ff1303f0664dc3b4519bb8d0cfbb
Leelsmuth <femianjorin@rocketmail.com>
2020-10-27 21:24:36 +0100

    added more tests

--- semver.test.js
+++ semver.test.js
@@ -5,3 +5,15 @@ test('throws error if passed with leading zero', () => {
     semverObj('01.11.10-alpha');
   }).toThrow();
 });
+
+test('test that functions return value for major version', () => {
+  expect(semverObj('1.11.10-alpha').major).toBe(1);
+});
+
+test('test that functions return value for minor version', () => {
+  expect(semverObj('1.11.10-alpha').minor).toBe(11);
+});
+
+test('test that functions return value for patch version and removes alpha, beta etc', () => {
+  expect(semverObj('1.11.10-alpha').patch).toBe(10);
+});
