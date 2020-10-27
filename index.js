5fd1a5730d9bd6ffb1d4bb4b135f5a0fae00d257
Leelsmuth <femianjorin@rocketmail.com>
2020-10-27 19:57:59 +0100

    determine precedence

---
+++ index.js
@@ -0,0 +1,50 @@
+const semverObj = require('./semver');
+
+function determinePrecedence(semver1, semver2) {
+
+  const s1 = semverObj(semver1);
+  const s2 = semverObj(semver2);
+
+
+  let versionCheck = false;
+
+  if (s1.major > s2.major) {
+
+    versionCheck = true;
+
+  } else if (s1.major === s2.major) {
+
+    if (s1.minor > s2.minor) {
+
+      versionCheck = true;
+
+    } else if (s1.minor === s2.minor) {
+
+      if (s1.patch > s2.patch) {
+
+        versionCheck = true;
+
+      } else {
+
+        versionCheck = false;
+
+      }
+
+    } else {
+
+      versionCheck = false;
+
+    }
+
+  } else {
+
+    versionCheck = false;
+
+  }
+
+  return versionCheck;
+}
+
+
+module.exports = determinePrecedence;
+// export default determinePrecedence
